1d5dc457e237adf989f677a66915d0646b715b26
tigrangrigoryan <tigran.grigoryan.mobile@gmail.com>
2022-08-04 13:39:57 +0400

    final

--- fund-course/src/hooks/useFetching.js
+++ fund-course/src/hooks/useFetching.js
@@ -9,7 +9,6 @@ export const useFetching = (callback) => {
       setIsLoading(true);
       await callback(...args);
     } catch (e) {
-      console.log(e);
       setError(e.message)
     } finally {
       setIsLoading(false);
